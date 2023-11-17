1cbf24c61bf305ba972f23cd969d131648c6bd00
Cam Bennett <cambennett72@gmail.com>
2023-11-17 14:38:30 -0500

    db

--- app/data/db.py
+++ app/data/db.py
@@ -1,4 +1,4 @@
 import databases 
 
 
-db = databases.Database(url="postgres://postgres:#gile4444@aa.cect20az9tah.us-east-1.rds.amazonaws.com:5432/aa")
+db = databases.Database(url="postgres:#gile4444@aa.cect20az9tah.us-east-1.rds.amazonaws.com:5432/aa")
